2e2c75f2c5177c7511ce057d132fccfe94173e13
billau_l <billau_l@modulotech.fr>
2024-12-02 11:22:15 +0100

    Day-2 avec un one-liner immonde check

--- day-2/part-1.rb
+++ day-2/part-1.rb
@@ -1,5 +1,5 @@
 puts(STDIN.read.split("\n")
           .map { _1.split(' ').map(&:to_i) }
           .map! {
-            (_1.sort == _1 || _1.sort.reverse == _1) && _1.each_cons(2).all? { |a| a[0] != a[1] && a.reduce(&:-).abs <= 3 }
+            _1.instance_eval { (sort == self || sort.reverse == self) && each_cons(2).all? { |a| a[0] != a[1] && a.reduce(&:-).abs <= 3 } }
           }.count{ _1 })
