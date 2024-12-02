380e8382f940c42eb8b71f031af0d3721eb460ac
billau_l <billau_l@modulotech.fr>
2024-12-02 11:11:24 +0100

    Day-2 avec un one-liner immonde check

--- day-2/part-2.rb
+++ day-2/part-2.rb
@@ -1,22 +1,9 @@
 puts(STDIN.read.split("\n")
           .map { _1.split(' ').map(&:to_i) }
           .map! do |ar|
-  res =
-    if (ar.sort == ar || ar.sort.reverse == ar) && ar.each_cons(2).map { |a| a.reduce(&:-).abs }.all? {|i| i >= 1 && i <= 3 }
-      'OK'
-    else
-      'KO'
-    end
-
-  if res == 'KO'
-    ar.length.times do |i|
-      arr = ar.clone
-      arr.delete_at(i-1)
-      if (arr.sort == arr || arr.sort.reverse == arr) && arr.each_cons(2).map { |a| a.reduce(&:-).abs }.all? {|i| i >= 1 && i <= 3 }
-        res = 'OK'
-        break
-      end
-    end
-  end
-  res
-end.count{ _1 == 'OK' })
+            (ar.instance_eval { (sort == self || sort.reverse == self) && each_cons(2).all? { |a| a[0] != a[1] && a.reduce(&:-).abs <= 3 } }) ||
+            ar.length.times.find do |i|
+              ar.each_with_index.reject { |_, n| n == i }.map(&:first)
+                .instance_eval { (sort == self || sort.reverse == self) && each_cons(2).all? { |a| a[0] != a[1] && a.reduce(&:-).abs <= 3 } }
+            end
+          end.count { _1 })
